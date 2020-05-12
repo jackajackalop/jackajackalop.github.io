d3c149257d5a8771a0def5ff13fdb16cc7401480
jackajackalop <yixinhe98@gmail.com>
2020-05-12 01:58:44 -0500

    fixed extra narrow thumbnails with narrow windows

--- js/gallery.js
+++ js/gallery.js
@@ -1,19 +1,20 @@
 $(function() {
     var selectedClass = "0";
     $("#gallery").fadeTo(100, 0.1);
-    $("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+    $("#gallery").find(":not(."+selectedClass+"):not(."+selectedClass+" *)").fadeOut();
     setTimeout(function() {
-        $("."+selectedClass).fadeIn().addClass('animation');
+        $("#gallery").find("."+selectedClass+", ."+selectedClass+" *").fadeIn();
         $("#gallery").fadeTo(300, 1);
     }, 300);
 
     $(".filter").click(function(){
         selectedClass = $(this).attr("data-rel");
         $("#gallery").fadeTo(100, 0.1);
-        $("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+        $("#gallery").find(":not(."+selectedClass+"):not(."+selectedClass+" *)").fadeOut();
         setTimeout(function() {
-            $("."+selectedClass).fadeIn().addClass('animation');
+            $("#gallery").find("."+selectedClass+", ."+selectedClass+" *").fadeIn();
             $("#gallery").fadeTo(300, 1);
         }, 300);
+
     });
 });
